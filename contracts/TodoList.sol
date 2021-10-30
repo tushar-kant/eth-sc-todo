4912c04cad207ed0910f97649821d84e3c38fa0f
Tushar kanta Nayak <85495427+tushar-kant@users.noreply.github.com>
2021-10-30 10:53:28 +0530

    Update TodoList.sol

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -23,7 +23,7 @@ contract TodoList {
   );
 
   constructor() public {
-    createTask("Check out dappuniversity.com");
+    createTask("Check out ");
   }
 
   function createTask(string memory _content) public {
